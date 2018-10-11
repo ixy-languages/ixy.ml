e21296a9a39531c6124077a62b4d3219ea28597c
Fabian Bonk <mail@fabianbonk.de>
2018-10-11 14:22:06 +0200

    bidirectional forward

--- app/fwd.ml
+++ app/fwd.ml
@@ -1,14 +1,22 @@
+let forward rx_dev tx_dev =
+  let rx = Ixy.rx_batch rx_dev 0 in
+  let rec loop rx =
+    match Ixy.tx_batch tx_dev 0 rx with
+    | [] -> ()
+    | rest -> loop rest in
+  let n = List.length rx in
+  if n > 0 then
+    Ixy.Log.info
+      "forwarding %d packets from %s to %s"
+      n
+      rx_dev.pci_addr
+      tx_dev.pci_addr;
+  loop rx
+
 let () =
-  let src = Ixy.create ~pci_addr:Sys.argv.(1) ~rxq:1 ~txq:1 in
-  let dst = Ixy.create ~pci_addr:Sys.argv.(2) ~rxq:1 ~txq:1 in
+  let dev_a = Ixy.create ~pci_addr:Sys.argv.(1) ~rxq:1 ~txq:1 in
+  let dev_b = Ixy.create ~pci_addr:Sys.argv.(2) ~rxq:1 ~txq:1 in
   while true do
-    let rx = Ixy.rx_batch src 0 in
-    let rec loop rx =
-      match Ixy.tx_batch dst 0 rx with
-      | [] -> ()
-      | rest -> loop rest in
-    let n = List.length rx in
-    if n > 0 then
-      Ixy.Log.info "forwarding %d packets" n;
-    loop rx
+    forward dev_a dev_b;
+    forward dev_b dev_a
   done
