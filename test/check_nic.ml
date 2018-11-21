0a66769ebe7c710d127351a3637cd0f262783b6f
Fabian Bonk <mail@fabianbonk.de>
2018-11-21 15:03:00 +0100

    update check_nic

--- test/check_nic.ml
+++ test/check_nic.ml
@@ -1,16 +1,17 @@
 open Core
 
+let usage () =
+  Ixy.Log.error "Usage: %s <pci_addr>" Sys.argv.(0)
+
 let () =
+  if Array.length Sys.argv <> 2 then
+    usage ();
   let pci_addr =
     match Ixy.PCI.of_string Sys.argv.(1) with
-    | None -> Ixy.Log.error "Usage: %s <pci_addr>" Sys.argv.(0)
+    | None -> usage ()
     | Some pci -> pci in
-  let ixy_dev =
-    try Ixy.create ~pci_addr ~rxq:1 ~txq:1 with
-    | Invalid_argument err ->
-      Printf.printf "Error: %s\nUsage: %s PCI-ADDR\n" err Sys.argv.(0);
-      exit 1 in
-  let speed, up = Ixy.check_link ixy_dev in
+  let dev = Ixy.create ~pci_addr ~rxq:1 ~txq:1 in
+  let speed, up = Ixy.check_link dev in
   begin match speed with
   | `SPEED_10G -> Printf.printf "speed: 10G\n"
   | `SPEED_1G -> Printf.printf "speed: 1G\n"
@@ -18,4 +19,4 @@ let () =
   | `SPEED_UNKNOWN -> Printf.printf "speed: UNKNOWN\n"
   end;
   Printf.printf "up: %b\n" up;
-  Ixy.reset ixy_dev
+  Ixy.reset dev
