ixy-languages/ixy.ml

commit 613e44231c3816f31252a2ce40e59cc656f02069

internal pagesize and cstruct layout independence

lib/memory.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <inttypes.h>
 #include <sys/mman.h>
 
@@ -9,18 +10,27 @@
 #include <caml/unixsupport.h>
 #include <caml/bigarray.h>
 
-CAMLprim value ixy_int64_of_addr(value cstruct) {
-    CAMLparam1(cstruct);
-    CAMLlocal1(ptr);
-    uint64_t offset = Long_val(Field(cstruct, 1));
-    ptr = caml_copy_int64(offset + (uint64_t) Caml_ba_data_val(Field(cstruct, 0)));
-    CAMLreturn(ptr);
+CAMLprim value ixy_pagesize(value v_unit) {
+    CAMLparam1(v_unit);
+    long pagesize = sysconf(_SC_PAGESIZE);
+    if (pagesize == -1)
+        uerror("sysconf", Nothing);
+    return caml_copy_int64(pagesize);
 }
 
-CAMLprim value ixy_mlock(value cstruct) {
-    CAMLparam1(cstruct);
-    void *ptr = Caml_ba_data_val(Field(cstruct, 0));
-    if (mlock(ptr, Long_val(Field(cstruct, 2))))
+CAMLprim value ixy_int64_of_addr(value v_buf, value v_off) {
+    CAMLparam2(v_buf, v_off);
+    CAMLlocal1(v_ptr);
+    size_t offset = Long_val(v_off);
+    v_ptr = caml_copy_int64((uint64_t) Caml_ba_data_val(v_buf) + offset);
+    CAMLreturn(v_ptr);
+}
+
+CAMLprim value ixy_mlock(value v_buf, value v_off, value v_size) {
+    CAMLparam3(v_buf, v_off, v_size);
+    void *ptr = Caml_ba_data_val(v_buf);
+    size_t offset = Long_val(v_off), size = Long_val(v_size);
+    if (mlock(ptr, offset + size))
         uerror("mlock", Nothing);
     CAMLreturn(Val_unit);
 }

lib/memory.ml
@@ -6,17 +6,23 @@ type dma_memory = {
   phys : Cstruct.uint64
 }
 
-external int64_of_addr : Cstruct.t -> Cstruct.uint64 = "ixy_int64_of_addr"
+external pagesize : unit -> int64 = "ixy_pagesize"
 
-external mlock : Cstruct.t -> unit = "ixy_mlock"
+external ixy_int64_of_addr :
+  Cstruct.buffer -> int -> Cstruct.uint64 = "ixy_int64_of_addr"
+
+let int64_of_addr Cstruct.{ buffer; off; _ } =
+  ixy_int64_of_addr buffer off
+
+external ixy_mlock : Cstruct.buffer -> int -> int -> unit = "ixy_mlock"
+
+let mlock Cstruct.{ buffer; off; len } =
+  ixy_mlock buffer off len
 
 let virt_to_phys virt =
   if Obj.is_int (Obj.repr virt) then
     raise (Invalid_argument "virt must be a pointer");
-  let pagesize =
-    match Unix.(sysconf PAGESIZE) with
-    | None -> error "cannot get pagesize"
-    | Some n -> n in
+  let pagesize = pagesize () in
   let fd = Unix.(openfile ~mode:[O_RDONLY] "/proc/self/pagemap") in
   let addr = int64_of_addr virt in
   let offset = Int64.(addr / pagesize * 8L) in

Makefile
@@ -1,4 +1,4 @@
-TEST=check_nic uname pci_config blink parse_pci_addr
+TEST=check_nic uname pci_config blink parse_pci_addr pagesize
 APPS=echo fwd pktgen
 
 all: ${TEST} ${APPS}

lib/memory.mli
@@ -1,3 +1,6 @@
+val pagesize : unit -> int64
+(** [pagesize ()] returns the size of a system page in bytes. *)
+
 type dma_memory = private {
   virt : Cstruct.t; (** DMA memory wrapped in a Cstruct.t *)
   phys : Cstruct.uint64 (** physical address of the beginning of the DMA memory buffer *)

test/dune
@@ -32,3 +32,10 @@
  (modes native)
  (libraries ixy)
  (modules parse_pci_addr))
+
+(executable
+ (name pagesize)
+ (public_name pagesize)
+ (modes native)
+ (libraries ixy)
+ (modules pagesize))

test/pagesize.ml
@@ -1,0 +1,2 @@
+let () =
+  Printf.printf "%Ld\n" (Ixy.Memory.pagesize ())

commit ca1c19a0d4ff5f9ec9b54f2a300db589d269a958

fix segmentation fault introduced in 613e44231c3816f31252a2ce40e59cc656f02069

lib/memory.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include <inttypes.h>
 #include <sys/mman.h>
 
@@ -9,14 +8,6 @@
 #include <caml/mlvalues.h>
 #include <caml/unixsupport.h>
 #include <caml/bigarray.h>
-
-CAMLprim value ixy_pagesize(value v_unit) {
-    CAMLparam1(v_unit);
-    long pagesize = sysconf(_SC_PAGESIZE);
-    if (pagesize == -1)
-        uerror("sysconf", Nothing);
-    return caml_copy_int64(pagesize);
-}
 
 CAMLprim value ixy_int64_of_addr(value v_buf, value v_off) {
     CAMLparam2(v_buf, v_off);

lib/memory.ml
@@ -6,7 +6,10 @@ type dma_memory = {
   phys : Cstruct.uint64
 }
 
-external pagesize : unit -> int64 = "ixy_pagesize"
+let pagesize () =
+  match Unix.(sysconf PAGESIZE) with
+  | None -> error "cannot get pagesize"
+  | Some n -> n
 
 external ixy_int64_of_addr :
   Cstruct.buffer -> int -> Cstruct.uint64 = "ixy_int64_of_addr"