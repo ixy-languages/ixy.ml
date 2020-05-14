453f908fed1b3e7f6da75a6f7126349b885b6f24
Fabian Bonk <mail@fabianbonk.de>
2020-05-14 23:42:03 +0200

    fix header_size

--- unikernel/echo-mirage/unikernel.ml
+++ unikernel/echo-mirage/unikernel.ml
@@ -4,7 +4,7 @@ module Main (S: Mirage_net.S) = struct
   let start net0 =
     S.listen
       net0
-      ~header_size:16
+      ~header_size:14
       (fun buf ->
         let size = Cstruct.len buf in
         S.write
